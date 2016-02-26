8554bd4f728fed5ffc0dd888930e10d002009836
Dan Turcza <datu925@gmail.com>
2016-02-26 09:07:27 -0600

    Add spotify api code

--- js/new.js
+++ js/new.js
@@ -27,4 +27,25 @@ $(function() {
       }
   }
 
+
+  var audio = new Audio();
+
+  function searchTracks("Back in Black artist: ACDC") {
+      $.ajax({
+          url: 'https://api.spotify.com/v1/search',
+          data: {
+              q: query,
+              type: 'track'
+          },
+          success: function (response) {
+              if (response.tracks.items.length) {
+                  var track = response.tracks.items[0];
+                  audio.src = track.preview_url;
+                  audio.play();
+                  communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+              }
+          }
+      });
+  }
+
 })
