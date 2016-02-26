1ae8d78f3eb2988a9cd5a7bd0c8d6d0271336c70
Dan Turcza <email": null>
2016-02-25 19:36:34 -0600

    Add video, icons, scroll, other dropdowns

--- js/new.js
+++ js/new.js
@@ -3,4 +3,16 @@ $(function() {
     event.preventDefault();
     $(this).parent().find(".cats").slideToggle();
   })
+
+  // $("h5.ban").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(this).parent().find(".acitem").slideToggle();
+  // })
+  $(".internal-nav").click(function() {
+      var destination = $(this).attr("href")
+      $('html, body').animate({
+          scrollTop: $(destination).offset().top - 60
+      }, 1000);
+  });
+
 })
