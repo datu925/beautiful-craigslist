0212d9e4f2f3e4cc42d86544348571921e41414d
Dan Turcza <email": null>
2016-02-25 22:18:06 -0600

    Add reappearing appearing other

--- js/new.js
+++ js/new.js
@@ -8,11 +8,23 @@ $(function() {
   //   event.preventDefault();
   //   $(this).parent().find(".acitem").slideToggle();
   // })
-  $(".internal-nav").click(function() {
+  $(".internal-nav").click(function(event) {
+      event.preventDefault();
       var destination = $(this).attr("href")
       $('html, body').animate({
           scrollTop: $(destination).offset().top - 60
       }, 1000);
   });
+
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    var content = $(".other-content *")
+      if ($("body").scrollTop() > content.offset().top + content.height() - window.innerHeight) {
+          $(".other-content *").css('visibility', 'visible'); // removeClass('hide');
+      } else {
+          $(".other-content *").css('visibility', 'hidden'); // addClass('hide');
+      }
+  }
 
 })
