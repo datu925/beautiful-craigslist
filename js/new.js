1c0545b24357d189ef208d45d02a56be10655dc3
Dan Turcza <email": null>
2016-02-26 11:31:03 -0600

    Add song picking functionality

--- js/new.js
+++ js/new.js
@@ -5,8 +5,8 @@ $(function() {
 
     playIt($(this).closest("form").find("input").val() ||
       "Welcome to the jungle");
-    $("body").addClass("loaded");
-    document.getElementById('video-player').play();
+    // $("body").addClass("loaded");
+    // document.getElementById('video-player').play();
   });
 
   $("h4.ban").on("click", function(event) {
@@ -33,9 +33,27 @@ $(function() {
       }
   }
 
+  $("#song-choice").on("click",".song-listing", function(event) {
+    console.log("not yet prevented!")
+    event.preventDefault();
+    console.log("prevented!")
+    var audio = new Audio();
+    var srcUrl = $(this).attr('href');
+    audio.src = srcUrl;
+    $("body").addClass("loaded");
+    document.getElementById('video-player').play();
+    audio.play();
+  })
+
 
   function playIt(query) {
-    var audio = new Audio();
+    // var audio = new Audio();
+
+    function generateSongHtml(song) {
+      var $listing = $("<li>");
+      var $anchor = $("<a class='song-listing' href=" + song.preview_url + ">" + song.artists[0].name + ": " + song.name + "</a>");
+      return $listing.append($anchor);
+    }
 
     function searchTracks(quer) {
         $.ajax({
@@ -46,12 +64,18 @@ $(function() {
             },
             success: function (response) {
               // console.log(response);
-              if (response.tracks.items.length) {
-                var track = response.tracks.items[0];
-                audio.src = track.preview_url;
-                audio.play();
-                // communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
-              }
+              var songs = response.tracks.items;
+              console.log(songs);
+              $("#song-container").html(songs.map(generateSongHtml));
+
+
+              // normal play first find option
+              // if (response.tracks.items.length) {
+              //   var track = response.tracks.items[0];
+              //   audio.src = track.preview_url;
+              //   audio.play();
+              //   // communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+              // }
             }
         });
     }
